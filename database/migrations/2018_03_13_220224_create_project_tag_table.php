c70b179c724353eb1b0f7faf2a39dff048a92714
Yauheni <tea001@tut.by>
2018-03-14 01:05:02 +0300

    tags select2 two

--- database/migrations/2018_03_13_220224_create_project_tag_table.php
+++ database/migrations/2018_03_13_220224_create_project_tag_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTagsTable extends Migration
+class CreateProjectTagTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,6 @@ class CreateTagsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tags', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->timestamps();
-        });
-
         Schema::create('project_tag', function (Blueprint $table) {
             $table->integer('project_id')->unsigned()->index();
             $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
@@ -37,7 +31,6 @@ class CreateTagsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tags');
         Schema::dropIfExists('project_tag');
     }
 }
